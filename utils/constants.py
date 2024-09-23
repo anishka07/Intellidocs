15f0ffe3900e9e97c897f212f355ab74954b3d45
anishka07 <anishkamukherjee237@gmail.com>
2024-09-23 12:51:40 +0545

    updates

--- utils/constants.py
+++ utils/constants.py
@@ -3,9 +3,11 @@ import os
 
 class PathSettings:
     PROJECT_DIR_PATH = os.path.dirname(os.getcwd())
-    PDF_FILE_PATH = os.path.join(PROJECT_DIR_PATH, 'pdfs')
+    PDF_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'pdfs')
     CHROMA_DB_PATH = os.path.join(PROJECT_DIR_PATH, 'db')
     LLM_PATH = os.path.join(PROJECT_DIR_PATH, 'LaMini-T5-738M')
+    PICKLE_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'pickle_models')
+    CSV_DB_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'CSV_db')
 
 
 class ConstantSettings:
