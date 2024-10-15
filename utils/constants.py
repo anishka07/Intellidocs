67336b240a2d9862a7be901f52f2e7da54e4750c
anishka07 <anishkamukherjee237@gmail.com>
2024-10-15 10:42:00 +0545

    added directory cleaner

--- utils/constants.py
+++ utils/constants.py
@@ -5,7 +5,6 @@ class PathSettings:
     PROJECT_DIR_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     PDF_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'pdfs')
     CHROMA_DB_PATH = os.path.join(PROJECT_DIR_PATH, 'db')
-    LLM_PATH = os.path.join(PROJECT_DIR_PATH, 'LaMini-T5-738M')
     PICKLE_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'pickle_models')
     CSV_DB_DIR_PATH = os.path.join(PROJECT_DIR_PATH, 'CSV_db')
 
---
+++ utils/dir_utils.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+from constants import PathSettings
+
+
+def clear_directory(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.remove(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+
+
+if __name__ == '__main__':
+    clear_directory(PathSettings.CSV_DB_DIR_PATH)
