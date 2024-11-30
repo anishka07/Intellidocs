7fd416cc215d45b42e541777f447018819bf1019
anishka07 <anishkamukherjee237@gmail.com>
2024-11-30 22:18:40 +0545

    added llm response

--- model/intellidocs_main.py
+++ model/intellidocs_main.py
@@ -1,5 +1,8 @@
 import os
 
+from ollama import ChatResponse
+from ollama import chat
+
 from model.intellidocs_rag_final.chunk_processor import tp_main
 from model.intellidocs_rag_final.embedding_process import embedding_process_main
 from model.intellidocs_rag_final.intellidocs_rag_constants import id_rag_pdf_path, id_pdf_name, \
@@ -7,6 +10,9 @@ from model.intellidocs_rag_final.intellidocs_rag_constants import id_rag_pdf_pat
 from model.intellidocs_rag_final.pdf_loader import pdf_loader_main
 from model.intellidocs_rag_final.retrieval_process import retriever_main
 from utils.constants import PathSettings
+
+
+query = "what are macro nutrients?"
 
 
 def intelli_docs_main(query: str, save_csv_name: str, save_csv_dir: str, rag_device: str):
@@ -27,12 +33,23 @@ def intelli_docs_main(query: str, save_csv_name: str, save_csv_dir: str, rag_dev
         embeddings_df_path=os.path.join(PathSettings.CSV_DB_DIR_PATH, save_csv_name),
         user_query=query
     )
-    return results
+    generated_response = llm_response(results)
+    return generated_response
+
+
+def llm_response(pdf_results: str):
+    response: ChatResponse = chat(model='mistral:latest', messages=[
+        {
+            'role': 'user',
+            'content': f'You are a RAG and this is the query the user asked {query} and these are the top 5 responses. I want you to summarize this and give me a structured response: {pdf_results}',
+        },
+    ])
+    return response['message']['content']
 
 
 if __name__ == '__main__':
     result = intelli_docs_main(
-        query="macro nutrients",
+        query=query,
         save_csv_name="mn.csv",
         save_csv_dir="CSV_db",
         rag_device="cpu"
