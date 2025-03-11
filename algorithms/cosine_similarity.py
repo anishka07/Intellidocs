fcab3f9f69e54c2e90aea1a19414886098ef43e8
anishka07 <anishkamukherjee237@gmail.com>
2025-03-11 22:33:15 +0545

    updates

--- algorithms/cosine_similarity.py
+++ algorithms/cosine_similarity.py
@@ -2,8 +2,4 @@ import numpy as np
 
 
 def cosine_similarity(a, b):
-    dot_product = np.dot(a, b)
-    magnitude_of_vector_a = np.linalg.norm(a)
-    magnitude_of_vector_b = np.linalg.norm(b)
-    similarity_score = dot_product / (magnitude_of_vector_a * magnitude_of_vector_b)
-    return similarity_score
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
