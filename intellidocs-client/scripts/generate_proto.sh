e5f3f1c771517ab847b43db67a5a16b4ce374408
anishka07 <anishkamukherjee237@gmail.com>
2025-07-23 13:00:53 +0545

    go backend initial commit

---
+++ intellidocs-client/scripts/generate_proto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Generate Go code from proto files
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    proto/intellidocs.proto
+
+echo "Proto files generated successfully!"
